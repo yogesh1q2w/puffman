792ac4821c9713e41784191cf6ec0d18ce17f3c6
yogesh1q2w <cs16b044@smail.iitm.ac.in>
2021-03-21 20:52:41 +0530

    seq. decompress done

--- seq_decompress.cpp
+++ seq_decompress.cpp
@@ -19,19 +19,14 @@ node *createTree(unsigned char *huffmanTreeInBits, uint &i) {
   unsigned char readBit = (huffmanTreeInBits[i / 8] >> (7 - (i % 8))) & 1;
   i++;
   if (readBit) {
-    cout << (int)readBit;
     char x = 0;
     for (uint j = 0; j < 8; j++) {
       unsigned char tokenBit = (huffmanTreeInBits[i / 8] >> (7 - (i % 8))) & 1;
       x = x | (tokenBit << (7 - j));
       i++;
     }
-    cout << x;
-    node *leaf = new node(x, NULL, NULL, true);
-    return leaf;
+    return new node(x, NULL, NULL, true);
   } else {
-    i++;
-    cout << (int)readBit;
     node *left = createTree(huffmanTreeInBits, i);
     node *right = createTree(huffmanTreeInBits, i);
     return new node(0, left, right, false);
@@ -48,12 +43,15 @@ node *readAndPrintTree(FILE *fptr, uint noOfLeaves) {
 }
 
 void readContent(FILE *fptr, uint fileSize, node *root, uint blockSize) {
-  FILE *tptr = fptr;
-  fseek(tptr, 0L, SEEK_END);
-  uint enc_size = ftell(tptr);
-  unsigned char encoding[enc_size];
-  uint readSize = fread(encoding, sizeof(unsigned char), enc_size, fptr);
-  cout << readSize << endl;
+
+  fpos_t ftell_val;
+  fgetpos(fptr, &ftell_val);
+  uint current = ftell(fptr);
+  fseek(fptr, 0L, SEEK_END);
+  uint enc_size = ftell(fptr) - current;
+  fsetpos(fptr, &ftell_val);
+  unsigned int encoding[(uint)ceil(enc_size/4.)];
+  uint readSize = fread(encoding, sizeof(unsigned int), ceil(enc_size/4.), fptr);
   unsigned char file[fileSize];
   uint i = 0;
   uint file_loc = 0;
@@ -61,22 +59,26 @@ void readContent(FILE *fptr, uint fileSize, node *root, uint blockSize) {
   while (1) {
     if (file_loc == fileSize)
       break;
-    uint bit = (1 & (encoding[i / 8] >> (7 - (i % 8))));
-    if (bit) {
-      temp = temp->l;
-    } else {
-      temp = temp->r;
-    }
+    uint bit = 1 & (encoding[i / 32] >> (31 - (i % 32)));
     if (temp->isleaf) {
       file[file_loc] = temp->a;
       file_loc++;
       temp = root;
-      cout << temp->a;
     } else if (i % blockSize == 0) {
       temp = root;
     }
+    if (!bit) {
+      temp = temp->l;
+    } else {
+      temp = temp->r;
+    }
+
+    i++;
   }
-  cout << endl;
+  FILE *optr;
+  optr = fopen("decompressed_out_seq", "wb");
+  fwrite(file, sizeof(unsigned char), fileSize, optr);
+
 }
 
 int main() {
@@ -92,7 +94,6 @@ int main() {
   cout << "Block size = " << blockSize << "\nNum Leaves = " << numLeaves
        << endl;
   node *root = readAndPrintTree(fptr, numLeaves);
-  cout << endl;
   readContent(fptr, fileSize, root, blockSize);
   return 0;
 }
